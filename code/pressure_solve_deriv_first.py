d765bf5e8a4ff783e90a6c4a6d5e5056ce97736c
Michael Megally <155863820+mmegally@users.noreply.github.com>
2026-06-06 19:24:21 -0700

    Update pressure_solve_deriv_first.py

--- code/pressure_solve_deriv_first.py
+++ code/pressure_solve_deriv_first.py
@@ -6,7 +6,7 @@ import re
 
 from pressure_estimation import solve_pressure
 
-BASE_DIRECTORY = "/scratch/users/mmegally/healthy_U_bend/24-procs"
+BASE_DIRECTORY = "CFD/data/dir"
 RHO = 1060.0  # Blood density [kg/m^3]
 MU = 0.004  # Blood dynamic viscosity [Pa*s]
 ARRAY_NAME = 'Material_Acceleration_pre_vti'
